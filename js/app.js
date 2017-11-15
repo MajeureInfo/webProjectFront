209f43d4dcb748bcad911560f4970cf2cd62a0b1
Pedro Duarte Bairao <pedro.bairao@gmail.com>
2017-11-15 16:48:33 +0100

    feat(tableData): retrieve the data from the DB and switch the light and ringer

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
+const url_api = "https://pure-island-76277.herokuapp.com/api/rooms"
+
 var roomsVue = new Vue({
   el: '#roomsVue',
   data: {
-    rooms: [
-      { id: 0,
-        light: {
-          level:32,
-          status:"OFF"
-        },
-        noise: {
-          level:32,
-          status:"ON"
-        }
-      },
-      { id: 1,
-        light: {
-          level:23,
-          status:"ON"
-        },
-        noise: {
-          level:15,
-          status:"OFF"
-        }
-      },
-    ]
+    rooms: []
+  },
+  methods: {
+    switchLight(room) {
+      axios.post(url_api +'/'+ room.id + "/switch-light")
+      .then(function(response) {
+        roomsVue.rooms=response.data;
+      })
+    },
+    switchRinger(room) {
+      axios.post(url_api +'/'+ room.id + "/switch-ringer")
+      .then(function(response) {
+        roomsVue.rooms=response.data;
+      })
+    }
   }
 })
+
+axios.get(url_api)
+.then(function(response) {
+  roomsVue.rooms = response.data;
+});
