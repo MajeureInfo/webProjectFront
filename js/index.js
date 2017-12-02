18c612844945064cdea067c4a6a1c14780ccd361
Julien Sulpis <julien.sulpis@etu.emse.fr>
2017-12-02 20:20:24 +0100

    feat(rooms): add a filter by building

--- js/index.js
+++ js/index.js
@@ -1,10 +1,76 @@
-const url_api = "https://pure-island-76277.herokuapp.com/api/rooms"
+// const url_api = "https://pure-island-76277.herokuapp.com/api"
+const url_api = "http://localhost:8080/api"
 
-// create the room vue for the table.
+function fillTableWithAllRooms() {
+  // request all the rooms and put the response in the attribute "data" of the roomsVue.
+  axios.get(url_api + "/rooms")
+  .then(function(response) {
+    roomsVue.rooms = response.data;
+  })
+  .then(function() {
+    $(".roomLine").each(function(index) {
+        $(this).delay(100*index).fadeIn();
+    });
+  });
+}
+
+function fillTableWithFilter(buildingId) {
+  // request the rooms of the chosen building and put the response in the attribute "data" of the roomsVue.
+  axios.get(url_api + "/buildings/" + buildingId)
+  .then(function(response) {
+    roomsVue.rooms = response.data.rooms;
+  })
+  .then(function() {
+    $(".roomLine").each(function(index) {
+        $(this).delay(100*index).fadeIn();
+    });
+  });
+}
+
+function fillTable(value) {
+  // fill the table depending on the value of the dropdown menu
+  if (Number.isInteger(parseInt(value))) {
+    fillTableWithFilter(value);
+  } else if (value == "All"){
+    fillTableWithAllRooms();
+  }
+}
+
+// create the Building Vue for the dropdown menu...
+var buildingVue = new Vue({
+  el: "#buildingFilter",
+  data: {
+    buildings: []
+  }
+});
+
+// ... and fill the menu with the buildings id
+$(function () {
+  axios.get(url_api + "/buildings")
+  .then(function(response) {
+    buildingVue.buildings = response.data;
+  });
+});
+
+// create and fill the room vue for the table.
 var roomsVue = createRoomsVue("roomsVue", url_api);
+fillTableWithAllRooms();
 
-// put the response from the request to the url in the attribute "data" of the given vue.
-axios.get(url_api)
-.then(function(response) {
-  roomsVue.rooms = response.data;
+
+// setup the filter on the building id
+$('#inlineBuildingSelector').change(function() {
+  $(".roomLine").hide();
+  fillTable(document.getElementById("inlineBuildingSelector").value);
+});
+
+// Toggle the building selector depending of the checkbox state
+$('#filterToggle').click(function() {
+  if (this.checked) {
+    $("#inlineBuildingSelector").attr('disabled', false);
+    fillTable(document.getElementById("inlineBuildingSelector").value);
+  }
+  else {
+    $("#inlineBuildingSelector").attr('disabled', true);
+    fillTableWithAllRooms();
+  }
 });
--- js/roomVue.js
+++ js/roomVue.js
@@ -1,25 +1,25 @@
 function createRoomsVue(id, url_api) {
   // create a vue to contain all the rooms
   // and define methods for switching light and ringer status
-  var genericVue = new Vue({
+  var roomVue = new Vue({
     el: "#" + id,
     data: {
       rooms: []
     },
     methods: {
       switchLight(room) {
-        axios.post(url_api +'/'+ room.id + "/switch-light")
+        axios.post(url_api +'/rooms/'+ room.id + "/switch-light")
         .then(function(response) {
-          genericVue.rooms = response.data;
+          roomVue.rooms = response.data;
         })
       },
       switchRinger(room) {
-        axios.post(url_api +'/'+ room.id + "/switch-ringer")
+        axios.post(url_api +'/rooms/'+ room.id + "/switch-ringer")
         .then(function(response) {
-          genericVue.rooms = response.data;
+          roomVue.rooms = response.data;
         })
       }
     }
   });
-  return genericVue;
+  return roomVue;
 }
