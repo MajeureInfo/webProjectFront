6b9590f88bf69c5d71df266ecfbf16e8739fa79d
Julien Sulpis <julien.sulpis@etu.emse.fr>
2017-12-02 20:20:24 +0100

    feat(rooms): add a filter by building

--- js/lights.js
+++ js/lights.js
@@ -1,17 +1,18 @@
-const url_api = "https://pure-island-76277.herokuapp.com/api/rooms"
+// const url_api = "https://pure-island-76277.herokuapp.com/api"
+const url_api = "http://localhost:8080/api"
 
 // create the vue for the dropdown menu in the formular.
-var dropdownVue = createRoomsVue("dropdown", url_api);
+var dropdownVue = createRoomsVue("dropdown", url_api + "/rooms");
 
 function refresh() {
   // refresh the list of the rooms with the response from the url.
-  axios.get(url_api + "/list-with-on-lights")
+  axios.get(url_api + "/rooms/list-with-on-lights")
   .then(function(response) {
     dropdownVue.rooms = response.data;
     // check if the list of the rooms with a light on is not empty.
     if (response.data.length == 0) {
-      $('select').attr('disabled', 'true');
-      $('#submit').attr('disabled', 'true');
+      $('select').attr('disabled', true);
+      $('#submit').attr('disabled', true);
       var alert = $('.alert-secondary');
       alert.text("All the lights are off !");
       alert.fadeIn();
@@ -24,9 +25,9 @@ refresh();
 $('form').on('submit', function(e) {
   // turn the light off when submitting the formular.
   e.preventDefault();
-  var roomId = document.getElementById("inlineFormSelect").value;
+  var roomId = document.getElementById("inlineRoomSelector").value;
   if (roomId != "Choose...") {
-    axios.post(url_api +'/'+ roomId + "/switch-light");
+    axios.post(url_api +'/rooms/'+ roomId + "/switch-light");
     // leave a bit of time to the server before asking to refresh
     setTimeout(function() {
       refresh();
