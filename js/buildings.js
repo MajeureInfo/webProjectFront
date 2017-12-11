0277473feeb39a4d254b203d80a78adbd679f6fe
pedrobairao <32354222+pedrobairao@users.noreply.github.com>
2017-12-11 19:37:35 +0100

    Update buildings.js

--- js/buildings.js
+++ js/buildings.js
@@ -1,4 +1,5 @@
-const url_api = "http://localhost:8080/api"
+// const url_api = "http://localhost:8080/api"
+const url_api = "https://pure-island-76277.herokuapp.com/api"
 
 function fillTableWithBuildings() {
   axios.get(url_api + "/buildings")
