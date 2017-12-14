6c198671886f01cde2c8920c9164b3ed3aca6e53
Pedro Duarte Bairao <pedro.bairao@gmail.com>
2017-12-14 12:04:41 +0100

    feat(mqtt): send MQTT messages when switching from the rooms table

--- js/roomVue.js
+++ js/roomVue.js
@@ -1,3 +1,5 @@
+var publisher = mqtt.connect("wss://publisher:publisher@m14.cloudmqtt.com:34219")
+
 function createRoomsVue(id, url_api) {
   // create a vue to contain all the rooms
   // and define methods for switching light and ringer status
@@ -8,12 +10,20 @@ function createRoomsVue(id, url_api) {
     },
     methods: {
       switchLight(room) {
+        //Send MQTT message to switch the light
+        console.log("Switch Light " + room.id);
+        publisher.publish( "room" + room.id + "/light", "switch");
+
         axios.post(url_api +'/rooms/'+ room.id + "/switch-light")
         .then(function(response) {
           roomVue.rooms = response.data;
         })
       },
       switchRinger(room) {
+        //Send MQTT message to switch the ringer
+        console.log("Switch Ringer " + room.id);
+        publisher.publish( "room" + room.id + "/ringer", "switch");
+
         axios.post(url_api +'/rooms/'+ room.id + "/switch-ringer")
         .then(function(response) {
           roomVue.rooms = response.data;
